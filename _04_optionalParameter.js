fd41bf98941db5cc0220620d81f4bd8644f3a043
Vijayalaxmi Kaleguddi <vkaleguddi@gmail.com>
2024-02-17 10:35:19 +0530

    typescipt file day 5

--- _04_optionalParameter.js
+++ _04_optionalParameter.js
@@ -1,7 +1,21 @@
-"use strict";
 function addnum(a, b) {
     if (typeof b !== 'undefined') {
         return a + b;
     }
 }
 console.log(addnum(1));
+// optional parameters
+function AddNum(num1, num2) {
+    if (typeof num2 !== 'undefined') {
+        return num1 + num2;
+    }
+}
+AddNum(1);
+// Default values 
+function display(id, name, role) {
+    if (role === void 0) { role = "Guest User"; }
+    console.log("Id", id);
+    console.log("Name", name);
+    console.log("Role", role);
+}
+display(1, 'Codemind', 'ITDept');
--- _04_optionalParameter.ts
+++ _04_optionalParameter.ts
@@ -7,3 +7,26 @@ function addnum(a: number, b?: number) {
 }
 
 console.log(addnum(1));
+
+// optional parameters
+function AddNum(num1: number, num2?: number) {
+    if (typeof num2 !== 'undefined') {
+        return num1 + num2;
+    }
+   
+}
+
+
+AddNum(1)
+
+// Default values 
+function display(id:number, name: string, role:string= "Guest User") {
+    console.log("Id",id);
+    console.log("Name",name);
+    console.log("Role",role);
+
+    
+}
+
+
+display(1, 'Codemind', 'ITDept');
